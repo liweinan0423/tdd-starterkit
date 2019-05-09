a3596b9eae1d4fae118643b88f1657d814f219e3
Li Weinan <li.weinan@rea-group.com>
2019-05-09 14:59:04 +0800

    1 is 1

---
+++ fizzbuzz.spec.js
@@ -0,0 +1,10 @@
+
+describe('fizzbuzz', () => {
+    it('should be 1 when fizzbuzz given 1', () => {
+        expect(fizzbuzz(1)).toBe(1);
+    })
+})
+
+function fizzbuzz(number) {
+    return 1
+}

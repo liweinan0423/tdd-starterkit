7620bd5a30a4b8e657a2bbbb599a0e4a6a9d01af
Li Weinan <li.weinan@rea-group.com>
2019-05-09 15:07:55 +0800

    2 is 2

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -3,8 +3,14 @@ describe('fizzbuzz', () => {
     it('should be 1 when fizzbuzz given 1', () => {
         expect(fizzbuzz(1)).toBe(1);
     })
+    it('should be 2 when fizzbuzz given 2', () => {
+        expect(fizzbuzz(2)).toBe(2);
+    })
 })
 
 function fizzbuzz(number) {
+    if (number == 2) {
+        return 2
+    }
     return 1
 }

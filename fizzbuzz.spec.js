ac5b50c2628b25db31d656cbc94c07a5783ab151
Li Weinan <li.weinan@rea-group.com>
2019-05-09 16:04:32 +0800

    finish fizzbuzz

--- fizzbuzz.spec.js
+++ fizzbuzz.spec.js
@@ -6,9 +6,23 @@ describe('fizzbuzz', () => {
     testFizz(3);
     testFizz(6);
     testFizz(9);
+    testBuzz(5);
+    testBuzz(10);
+    testBuzz(25);
+    testFizzBuzz(15);
+    testFizzBuzz(30);
 })
 
-
+function testFizzBuzz(number) {
+    it(`should be fizzbuzz when fizzbuzz given ${number}`, () => {
+        expect(fizzbuzz(number)).toBe("fizzbuzz")
+    })
+}
+function testBuzz(number) {
+    it(`should be buzz when fizzbuzz given ${number}`, () => {
+        expect(fizzbuzz(number)).toBe("buzz")
+    })
+}
 function testFizz(number) {
     it(`should be fizz when fizzbuzz given ${number}`, () => {
         expect(fizzbuzz(number)).toBe("fizz")
@@ -21,5 +35,12 @@ function testNormalNumber(number) {
 }
 
 function fizzbuzz(number) {
-    return number % 3 === 0 ? 'fizz' : number;
+    if (number % 3 === 0 && number % 5 !== 0) {
+        return 'fizz'
+    } else if (number % 5 === 0 && number % 3 !== 0) {
+        return 'buzz'
+    } else if (number % 5 === 0 && number % 3 === 0) {
+        return 'fizzbuzz'
+    }
+    return number;
 }
